2498492eba93f6a73f862f8823f20a4062351c79
Vishnu <vishnuindrabalan@gmail.com>
2020-09-13 05:28:23 +0530

    testing build issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
 import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 function Main() {
   React.useEffect(() => {
@@ -17,7 +18,9 @@ function Main() {
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
